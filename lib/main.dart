23382388e05e41eaca96a363b856dc3d78769ee7
krmaazha <krmaazha@gmail.com>
2026-06-03 19:40:06 +0500

    Added Firebase packages and configured Windows CMake settings

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,18 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:window_manager/window_manager.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'app.dart';
 import 'core/notifications/notification_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   if (Platform.isWindows || Platform.isLinux) {
     sqfliteFfiInit();
